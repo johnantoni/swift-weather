17a1131959f8b02e1b5c3379cebedc2a20a700b6
John Griffiths <john@uxgent.co>
2015-04-09 20:53:13 -0400

    add podfile header.

---
+++ Weather/PodfileImporter.h
@@ -0,0 +1,10 @@
+//
+//  PodfileImporter.h
+//  Weather
+//
+//  Created by John Griffiths on 2015-04-09.
+//  Copyright (c) 2015 Tony. All rights reserved.
+//
+
+#import "AFNetworking.h"
+#import "FMDB.h"
